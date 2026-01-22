12757c031a8ae529b944d1818ca815cf69e2e50f
skuzzis <andreieurt@gmail.com>
2026-01-22 05:12:52 +0200

    update(classes): stuff

--- public/globalvars_base.h
+++ public/globalvars_base.h
@@ -13,8 +13,8 @@
 
 enum GlobalVarsUsageWarning_t
 {
-	GV_RENDERTIME_CALLED_DURING_SIMULATION,
-	GV_CURTIME_CALLED_DURING_RENDERING
+    GV_RENDERTIME_CALLED_DURING_SIMULATION,
+    GV_CURTIME_CALLED_DURING_RENDERING
 };
 
 typedef void (*FnGlobalVarsWarningFunc)(GlobalVarsUsageWarning_t);
@@ -26,66 +26,70 @@ class CGlobalVarsBase
 {
 public:
 
-	CGlobalVarsBase();
+    CGlobalVarsBase();
 
 public:
-	// Absolute time (per frame still - Use Plat_FloatTime() for a high precision real time 
-	//  perf clock, but not that it doesn't obey host_timescale/host_framerate)
-	float realtime;
-	// Absolute frame counter - continues to increase even if game is paused
-	int framecount;
+    // Absolute time (per frame still - Use Plat_FloatTime() for a high precision real time 
+    //  perf clock, but not that it doesn't obey host_timescale/host_framerate)
+    float realtime;
+    // Absolute frame counter - continues to increase even if game is paused
+    int framecount;
 
-	// Non-paused frametime
-	float absoluteframetime;
-	float absoluteframestarttimestddev;
+    // Non-paused frametime
+    float absoluteframetime;
+    float absoluteframestarttimestddev;
 
-	int maxClients;
+    int maxClients;
 
-	// zer0k: Command queue related
-	int unknown1;
-	int unknown2;
-	int unknown3;
-	int unknown4;
-	int unknown5;
+    // zer0k: Command queue related
+    int unknown1;
+    int unknown2;
+    int unknown3;
+    int unknown4;
+    int unknown5;
 
-	FnGlobalVarsWarningFunc m_pfnWarningFunc;
+    FnGlobalVarsWarningFunc m_pfnWarningFunc;
 
 
-	// Current time 
-	//
-	// On the client, this (along with tickcount) takes a different meaning based on what
-	// piece of code you're in:
-	// 
-	//   - While receiving network packets (like in PreDataUpdate/PostDataUpdate and proxies),
-	//     this is set to the SERVER TICKCOUNT for that packet. There is no interval between
-	//     the server ticks.
-	//     [server_current_Tick * tick_interval]
-	//
-	//   - While rendering, this is the exact client clock 
-	//     [client_current_tick * tick_interval + interpolation_amount]
-	//
-	//   - During prediction, this is based on the client's current tick:
-	//     [client_current_tick * tick_interval]
-	float curtime;
-	
-	// Time spent on last server or client frame (has nothing to do with think intervals)
-	float frametime;
+    // Current time 
+    //
+    // On the client, this (along with tickcount) takes a different meaning based on what
+    // piece of code you're in:
+    // 
+    //   - While receiving network packets (like in PreDataUpdate/PostDataUpdate and proxies),
+    //     this is set to the SERVER TICKCOUNT for that packet. There is no interval between
+    //     the server ticks.
+    //     [server_current_Tick * tick_interval]
+    //
+    //   - While rendering, this is the exact client clock 
+    //     [client_current_tick * tick_interval + interpolation_amount]
+    //
+    //   - During prediction, this is based on the client's current tick:
+    //     [client_current_tick * tick_interval]
+    float curtime;
 
-	// zer0k: Command queue + interpolation related 
-	float unknown6;
-	float unknown7;
+    // Time spent on last server or client frame (has nothing to do with think intervals)
+    float frametime;
 
-	bool m_bInSimulation;
-	bool m_bEnableAssertions;
+    // zer0k: Command queue + interpolation related 
+    float unknown6;
+    float unknown7;
 
-	// Simulation ticks - does not increase when game is paused
-	int tickcount;
+    bool m_bInSimulation;
+    bool m_bEnableAssertions;
 
-	int unknown8;
-	int unknown9;
-	
-	// Non-zero when during movement processing, it's the part after the decimal point of the "when" field in player's subtick moves.
-	float m_flSubtickFraction;
+    // Simulation ticks - does not increase when game is paused
+    int tickcount;
+
+    int unknown8;
+    int unknown9;
+
+    // Non-zero when during movement processing, it's the part after the decimal point of the "when" field in player's subtick moves.
+    float m_flSubtickFraction;
+
+    float IntervalPerTick;
+
+    float Unknown;
 };
 
 inline CGlobalVarsBase::CGlobalVarsBase()
