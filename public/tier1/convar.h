f1b56ee3e8276cb2c796aa8a7c554349e760b7db
skuzzis <andreieurt@gmail.com>
2026-09-18 21:00:29 +0300

    feat(convar): Update EConVarType

--- public/tier1/convar.h
+++ public/tier1/convar.h
@@ -67,23 +67,24 @@ struct CSplitScreenSlot
 //-----------------------------------------------------------------------------
 enum EConVarType : int16_t
 {
-    EConVarType_Invalid = -1,
-    EConVarType_Bool,
-    EConVarType_Int16,
-    EConVarType_UInt16,
-    EConVarType_Int32,
-    EConVarType_UInt32,
-    EConVarType_Int64,
-    EConVarType_UInt64,
-    EConVarType_Float32,
-    EConVarType_Float64,
-    EConVarType_String,
-    EConVarType_Color,
-    EConVarType_Vector2,
-    EConVarType_Vector3,
-    EConVarType_Vector4,
-    EConVarType_Qangle,
-    EConVarType_MAX
+	EConVarType_Invalid = -1,
+	EConVarType_Bool,
+	EConVarType_Int16,
+	EConVarType_UInt16,
+	EConVarType_Int32,
+	EConVarType_UInt32,
+	EConVarType_Int64,
+	EConVarType_UInt64,
+	EConVarType_Float32,
+	EConVarType_Float64,
+	EConVarType_String,
+	EConVarType_Color,
+	EConVarType_Vector2,
+	EConVarType_Vector3,
+	EConVarType_Vector4,
+	EConVarType_Qangle,
+	EConVarType_VectorWS,
+	EConVarType_MAX
 };
 
 template<typename T>
